278b7b10f87ea2c933ea92062a76147e90e11296
Ismail-Nd <ndismail007@gmail.com>
2026-06-22 22:57:39 +0200

    Added a script that checks user input data in numbers and calcualates it in hours, minutes and seconds

---
+++ time_machine.py
@@ -0,0 +1,16 @@
+## This program calcualtes user inputs and returns complete time in days, hours, minutes, seconds.
+
+## We will start by asking the user to enter the number of seconds.
+
+data = int(input("Enter the number of seconds: "))
+
+## We will then calculate the number of days, hours, minutes, seconds.
+
+hours = data // 3600 
+data = data % 3600
+minutes = data // 60
+data = data % 60
+seconds = data
+
+print(f"The time is {hours} hour(s), {minutes} minute(s), {seconds} second(s).")
+
