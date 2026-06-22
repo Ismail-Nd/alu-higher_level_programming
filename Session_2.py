6a3308625adff927b6187772ba10cc7158bcf041
Ismail-Nd <ndismail007@gmail.com>
2026-06-22 18:07:48 +0200

    Session-2 working with print function

---
+++ Session_2.py
@@ -0,0 +1,18 @@
+a = 4 
+b = str(4)
+
+import smtplib
+from email.message import EmailMessage
+
+# print(f'The type of this variable is {type(a)}')
+
+#num1 = int(input('Enter num1: '))
+#num2 = int(input('Enter num2: '))
+
+#bitwise_and = num1 & num2
+# print(f'The Floor division is: {num1 // num2}')  
+# print(f'Bitwise AND is: {bitwise_and}')         
+# print(f'The Addition is: {num1 + num2}')          
+# print(f'The Subtraction is: {num1 - num2}')       
+# print(f'The Division is: {num1 / num2}')         
+# print(f'The Modulus is: {num1 % num2}')
