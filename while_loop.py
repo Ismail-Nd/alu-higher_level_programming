ddb1b8645a4ed4aa1392e700aaa0797e768128e4
Ismail-Nd <ndismail007@gmail.com>
2026-06-23 09:54:39 +0200

    Aded for loop

--- while_loop.py
+++ while_loop.py
@@ -1,8 +1,28 @@
 
-loop = 0 
+# loop = 0 
 
-while loop < 3:
-    loop += 1
-    print(f'Car is on loop {loop}')
+# while loop < 3:
+#     loop += 1
+#     print(f'Car is on loop {loop}')
 
-print(f'Car is off it has reached its destination {loop}')
+# print(f'Car is off it has reached its destination {loop}')
+
+students = ['John', 'Jane', 'Jim', 'Jill']
+
+# for student in students:
+#     print(f'Student is {student}')
+
+# print(f'All students have been printed')
+
+#Using break and continue
+
+for student in students:
+    print(f'Searching John...')
+    if student == 'John':
+        print(f'John found')
+        break
+    else:
+        print(f'John not found')
+        continue
+
+
